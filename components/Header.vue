3c23c6afac2f749aef9ed7c3c5a84fe2a946fa98
fran-dea <102698965+fran-dea@users.noreply.github.com>
2022-10-11 21:31:34 -0300

    Cria header
    Signed-off-by: fran-dea <102698965+fran-dea@users.noreply.github.com>

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,65 @@
 <template>
   <v-row cols="12" style="background-color: #dae3f0">
     <v-container>
-      <h1>Header</h1>
-      <p>Change me</p>
+      <v-carousel
+        cycle
+        height="400"
+        hide-delimiter-background
+        :show-arrows="false"
+      >
+        <v-carousel-item v-for="(slide, i) in Main" :key="i">
+          <v-row class="carrossel" justify="left">
+            <v-container class="texto">
+              <div>
+                <p id="sub">{{ Pitico[i] }}</p>
+
+                <p id="big">{{ slide }}</p>
+                <p id="infolink">{{ ref[i] }}</p>
+              </div>
+              <div>
+                <Tux />
+              </div>
+            </v-container>
+          </v-row>
+        </v-carousel-item>
+      </v-carousel>
     </v-container>
   </v-row>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      Main: ["Rede GNU-Linux IME-USP", "Crie a sua conta"],
+      Pitico: ["Bem vindo à", "Conta"],
+      ref: ["Mais informações", "Clique aqui para criar sua conta"],
+    };
+  },
+};
+</script>
 
-<style scoped></style>
+<style scoped>
+.texto {
+  display: flex;
+  color: rgba(5, 22, 30, 1);
+  font-size: 5rem;
+  font-family: "Ubuntu";
+  font-weight: bolder;
+  column-count: 2;
+  column-gap: 100px;
+  margin: 25px;
+}
+#sub {
+  font-size: 2rem;
+  margin-top: 25px;
+  font-weight: normal;
+}
+#big {
+  line-height: 0.9;
+}
+#infolink {
+  font-size: 2rem;
+  font-weight: normal;
+}
+</style>
---
+++ components/tux.vue
@@ -0,0 +1,17 @@
+<template>
+  <img
+    class="tux"
+    alt="Tux"
+    src="/Tux.svg"
+    :aspect-ratio="16 / 9"
+    :width="250"
+  />
+</template>
+
+<style>
+.tux {
+  color: rgba(5, 22, 30, 1);
+  margin-top: 30px;
+  margin-right: 100px;
+}
+</style>
