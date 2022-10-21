26b168dd4d1875fe5f1d107d3952d933dee2c3c4
Jose Lucas Silva Mayer <josemayer@usp.br>
2022-10-21 16:24:50 -0300

    Atualiza formatação para dicionário, adiciona botões e corrige designs pontuais
    Signed-off-by: Jose Lucas Silva Mayer <josemayer@usp.br>

--- components/Header.vue
+++ components/Header.vue
@@ -1,28 +1,32 @@
 <template>
-  <v-row cols="12" style="background-color: #dae3f0">
+  <v-row style="background-color: #dae3f0">
     <v-container>
-      <v-carousel
-        cycle
-        height="400"
-        hide-delimiter-background
-        :show-arrows="false"
-      >
-        <v-carousel-item v-for="(slide, i) in Main" :key="i">
-          <v-row class="carrossel" justify="left">
+      <v-row>
+        <v-col cols="8" align-self="center" align="left">
+        <v-carousel
+          cycle
+          height="400"
+          hide-delimiter-background
+          :show-arrows="false"
+        >
+          <v-carousel-item v-for="slide in slides" :key="slide">
             <v-container class="texto">
-              <div>
-                <p id="sub">{{ Pitico[i] }}</p>
-
-                <p id="big">{{ slide }}</p>
-                <p id="infolink">{{ ref[i] }}</p>
-              </div>
-              <div>
-                <Tux />
-              </div>
+              <h3 class="sub">{{ slide.smallIntro }}</h3>
+              <h1 class="big">{{ slide.mainText }}</h1>
+              <template v-for="button in slide.buttons">
+                <v-btn v-if="!button.external" :to="button.link" :color="button.color" class="infolink" flat outlined>
+                  {{ button.text }}
+                </v-btn>
+                <v-btn v-else :href="button.link" :color="button.color" class="infolink" flat outlined>
+                  {{ button.text }} 
+                </v-btn>
+              </template>
             </v-container>
-          </v-row>
-        </v-carousel-item>
-      </v-carousel>
+          </v-carousel-item>
+        </v-carousel>
+        </v-col>
+        <v-col cols="4" align-self="center" align="right"><Tux /></v-col>
+      </v-row>
     </v-container>
   </v-row>
 </template>
@@ -31,35 +35,65 @@
 export default {
   data() {
     return {
-      Main: ["Rede GNU-Linux IME-USP", "Crie a sua conta"],
-      Pitico: ["Bem vindo à", "Conta"],
-      ref: ["Mais informações", "Clique aqui para criar sua conta"],
-    };
-  },
-};
+      slides: [
+        {
+          smallIntro: "Bem vindo(a) à",
+          mainText: "Rede GNU-Linux IME-USP",
+          buttons: [
+            {
+              text: "Mais informações",
+              external: false,
+              link: "/inspire",
+              color: "rgba(5, 22, 30, 1)"
+            },
+            {
+              text: "Logue-se",
+              external: false,
+              link: "/login",
+              color: "rgba(0, 0, 0, 1)"
+            }
+          ]
+        },
+        {
+          smallIntro: "Conta",
+          mainText: "Crie a sua conta",
+          buttons: [
+            {
+              text: "Clique aqui",
+              external: true,
+              link: "https://linux.ime.usp.br",
+              color: "rgba(5, 22, 30, 1)"
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style scoped>
 .texto {
-  display: flex;
   color: rgba(5, 22, 30, 1);
-  font-size: 5rem;
   font-family: "Ubuntu";
   font-weight: bolder;
-  column-count: 2;
-  column-gap: 100px;
-  margin: 25px;
+  padding: 30px 0px;
 }
-#sub {
+h1 {
+  font-size: 5rem;
+  padding-top: 10px;
+  padding-bottom: 50px;
+}
+.sub {
   font-size: 2rem;
-  margin-top: 25px;
   font-weight: normal;
 }
-#big {
+.big {
   line-height: 0.9;
 }
-#infolink {
-  font-size: 2rem;
-  font-weight: normal;
+.infolink {
+  font-size: 1rem;
+  margin-right: 10px;
+  margin-top: 10px;
 }
 </style>
