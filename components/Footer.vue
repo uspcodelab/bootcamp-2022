56d5bbf7ec95c3b32ff643d4a2a5e75c447cd9f3
Jose Lucas Silva Mayer <josemayer@usp.br>
2022-10-26 23:08:06 -0300

    Corrige inconsistências de grade e incorpora mudanças sugeridas
    Remove incoerência do v-container com o restante das páginas, adiciona a tag v-row, corrige breakpoints, corrige ícones,
    adiciona flex-helpers para gerenciar as classes flex e faz mudanças pontuais nos textos e formatação.
    
    Signed-off-by: Jose Lucas Silva Mayer <josemayer@usp.br>

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,38 +1,40 @@
 <template>
-  <v-container id="FooterContainer">
-    <v-col class="FooterCol" xs="12" sm="12" md="4" lg="4" xl="4" col="12">
-      <div class="DivFooter-0">
-        <p class="TextFooter">In memorian</p>
-        <p class="TextFooter">Rogério Theodoro Brito</p>
-        <p class="TextFooter">Sem o qual esta rede não estaria aqui.</p>
-      </div>
-    </v-col>
-
-    <v-col class="FooterCol" xs="12" sm="12" md="4" lg="4" xl="4" col="12">
-      <div class="DivFooter-0">
-        <p class="TextFooter">
-          Endereço: Rua do Matão, 1010, Bloco A, Sala 125 -
-        </p>
-        <p class="TextFooter">Cidade Universitária SP</p>
-      </div>
-    </v-col>
-    <v-col class="FooterCol" xs="12" sm="12" md="12" lg="4" xl="4" col="12">
-      <div class="DivFooter-1">
-        <div class="DivFooter-2">
-          <div class="DivFooter-3">
-            <img id="Telefone" alt="Icone Telefone" src="/telefone.png" />
-            <p class="TextFooter" style="margin: 10px">
-              Telefone: {{ telefone }}
-            </p>
-          </div>
-          <div class="DivFooter-3">
-            <img id="Envelope" alt="Icone Envelope" src="/envelope.png" />
-            <p class="TextFooter">Email: {{ email }}</p>
-          </div>
+  <v-container>
+    <v-row align="center">
+      <v-col cols="12" md="4">
+        <div class="d-flex flex-column align-center">
+          <p><i>In memorian:</i></p>
+          <span><b>Rogério Theodoro Brito</b></span>
+          <span>Sem o qual esta rede não estaria aqui.</span>
         </div>
-        <ImeLogo />
-      </div>
-    </v-col>
+      </v-col>
+
+      <v-col cols="12" md="4">
+        <div class="d-flex flex-column align-center">
+          <span>
+            Endereço: Rua do Matão, 1010, Bloco A, Sala 125
+          </span>
+          <span>Cidade Universitária - USP, São Paulo/SP</span>
+        </div>
+      </v-col>
+      <v-col cols="12" md="4">
+        <div class="d-flex flex-row align-center DivFooter-1">
+          <div class="d-flex flex-column align-center">
+            <div class="d-flex flex-row align-center">
+              <v-icon class="mx-2" id="Telefone" color="white">mdi-phone</v-icon>
+              <span>
+                Telefone: {{ telefone }}
+              </span>
+            </div>
+            <div class="d-flex flex-row align-center">
+              <v-icon class="mx-2" id="Envelope" color="white">mdi-email</v-icon>
+              <span>Email: {{ email }}</span>
+            </div>
+          </div>
+          <ImeLogo />
+        </div>
+      </v-col>
+      </v-row>
   </v-container>
 </template>
 
@@ -48,61 +50,15 @@ export default {
 </script>
 
 <style scoped>
-#FooterContainer {
-  display: flex;
-  flex-wrap: wrap;
-  min-width: 100%;
-  justify-content: center;
-  padding: 0;
-}
-
-.FooterCol {
-  display: flex;
-  width: 100%;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  padding: 0;
-}
-
-.DivFooter-0 {
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  flex-direction: column;
-  align-items: center;
-}
-
 .DivFooter-1 {
-  display: flex;
-  width: 100%;
-  flex-direction: row;
-  align-items: center;
   justify-content: space-evenly;
 }
 
-.DivFooter-2 {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.DivFooter-3 {
-  display: flex;
-  flex-direction: row;
-}
-
-.TextFooter {
-  margin: 0px;
-}
-
 #Telefone {
-  margin: 15px -4px 0 0;
-  height: 15px;
+  text-shadow: 2px 2px 6px black;
 }
 
 #Envelope {
-  margin: 5px 8px 0 0;
-  height: 14px;
+  text-shadow: 2px 2px 6px black;
 }
 </style>
