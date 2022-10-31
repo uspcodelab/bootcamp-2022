3288086ddeaccce2f87b636402c4f1a329e17933
Jose Lucas Silva Mayer <josemayer@usp.br>
2022-10-31 15:04:49 -0300

    Generaliza o componente de texto do footer com slots e adiciona flex-helpers
    Signed-off-by: Jose Lucas Silva Mayer <josemayer@usp.br>

--- components/FooterTexto.vue
+++ components/FooterTexto.vue
@@ -1,40 +1,15 @@
 <template>
-  <div class="FooterDiv">
-    <span v-if="linha1 != ''" v-html="linha1"> </span>
-    <span v-if="linha2 != ''" v-html="linha2" id="nome"> </span>
-    <span v-if="linha3 != ''" v-html="linha3"> </span>
+  <div class="d-flex flex-row justify-space-between">
+    <div class="d-flex flex-column align-center text-center FooterDiv">
+       <slot name="left"></slot>
+    </div>
+    <slot name="right"></slot>
   </div>
 </template>
-<script>
-export default {
-  props: {
-    linha1: {
-      type: String,
-      default: "",
-    },
-    linha2: {
-      type: String,
-      default: "",
-    },
-    linha3: {
-      type: String,
-      default: "",
-    },
-  },
-};
-</script>
 
 <style scoped>
 .FooterDiv {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex: 1;
   justify-content: space-evenly;
-  text-align: center;
-}
-
-#nome > * {
-  margin: 100px;
 }
 </style>
