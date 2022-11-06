18db36cb7ed5de389f807a9eea828029fb5c0a32
Jose Lucas Silva Mayer <josemayer@usp.br>
2022-11-06 01:57:15 -0300

    Corrige borda inferior de card do FAQ
    Signed-off-by: Jose Lucas Silva Mayer <josemayer@usp.br>

--- frontend/components/FAQCard.vue
+++ frontend/components/FAQCard.vue
@@ -1,17 +1,18 @@
 <template>
-  <v-expansion-panels accordion flat>
+  <v-expansion-panels flat>
     <v-expansion-panel
-      class="py-1 rounded-lg black--text question"
+      class="rounded-lg black--text question"
       active-class="active"
+      elevation="0"
     >
       <v-expansion-panel-header v-slot="{ open }" expand-icon="">
-        <v-layout justify-space-between align-center>
+        <v-layout justify-space-between align-center py-1>
           {{ question }}
           <v-icon v-if="!open" color="black">mdi-chevron-down</v-icon>
           <v-icon v-else color="white">mdi-chevron-up</v-icon>
         </v-layout>
       </v-expansion-panel-header>
-      <v-expansion-panel-content class="rounded-lg rounded-t-0">
+      <v-expansion-panel-content color="#f2f2f2" class="rounded-lg rounded-t-0">
         {{ answer }}
       </v-expansion-panel-content>
     </v-expansion-panel>
@@ -31,7 +32,6 @@ export default {
 .v-expansion-panel-content {
   white-space: pre-line;
   color: #797979;
-  background-color: #f2f2f2;
 }
 .v-expansion-panel {
   margin: 8px;
