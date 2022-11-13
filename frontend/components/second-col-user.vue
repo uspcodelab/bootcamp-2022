c3a1a439d809d5a729fc69db263cb8dc5ee26861
jppaulo11 <jppaulo11@usp.br>
2022-11-13 00:05:46 -0300

    atualiza user-page

--- frontend/components/second-col-user.vue
+++ frontend/components/second-col-user.vue
@@ -1,22 +1,29 @@
 <template>
-  <v-card class="d-flex flex-column" align-center flat color="gray" light>
+  <v-card 
+    class="d-flex flex-column justify-space-between" 
+    align-center 
+    flat 
+    color="gray" 
+    light
+    min-height="500"
+  >
       <h2> Sua conta de usuário </h2>
-      <p style="font-size: 20px; margin: 10px"> Detalhes da conta </p>
+      <p style="font-size: 20px; margin: -15px 0 -15px 0; padding: 0"> Detalhes da conta </p>
       <div>
         <h3> Login </h3>
-        <span> username </span>
+        <span> {{ username }}</span>
       </div> 
-      <div> 
-        <h3> Email de contato </h3>
-        <span> username </span>
+      <div class="d-flex flex-column"> 
+        <h3> E-mails de contato </h3>
+        <span v-for="email of emails"> {{ email }} </span>
       </div>
       <div>      
         <h3> Shell </h3>
-        <span> username </span>
+        <span> /bin/bash </span>
       </div>
       <div>
         <h3> Grupo </h3>
-        <span> username </span>
+        <span> {{ group }} </span>
       </div>
       <div>
         <h3> Website </h3>
@@ -29,14 +36,13 @@
   export default {
     props: {
       username: String,
+      emails: Array,
       group: String,
-      email: String,
     }
   }
 </script>
-
 <style scoped>
-  div {
-    margin-top: 20px;
+  h3 {
+    margin-bottom: 6px;
   }
 </style>
