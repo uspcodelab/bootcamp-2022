59af5da0a723aa5bfda3b7db3fb1148297f1cbcf
fran-dea <102698965+fran-dea@users.noreply.github.com>
2022-11-15 00:46:25 -0300

    conserta conflitos

--- frontend/pages/user.vue
+++ frontend/pages/user.vue
@@ -2,7 +2,7 @@
   <v-col>
     <Header-padrao icon="mdi-account-circle" :text="username" />
     <v-container>
-      <p style="font-size: 22px; margin-top: 25px">Olá, {{ username }}!</p>
+      <p style="font-size: 22px; margin: 20px 0 20px 0"> Olá, {{ username }}! </p>
       <v-row>
         <v-col>
           <first-col-user
@@ -12,7 +12,11 @@
           />
         </v-col>
         <v-col>
-          <second-col-user />
+          <second-col-user 
+            :institute="institute"
+            :degree="degree"
+            :SSH="SSH"
+          />
         </v-col>
       </v-row>
     </v-container>
@@ -25,6 +29,9 @@ export default {
       username: "Pedrinho",
       emails: ["pedrinho_gameplays@gmail.com", "pedro_bem_profissional@usp.br"],
       group: "bcc (20)",
+      institute: "Instituto de Matemática e Estatítica",
+      degree: "Aluno de graduação",
+      SSH: true,
     };
   },
 };
