13d7f97c235858b65c5c8bb57de0852406e53e05
Jose Lucas Silva Mayer <josemayer@usp.br>
2022-11-19 20:27:45 -0300

    Adiciona instâncias da seção dos slides
    Signed-off-by: Jose Lucas Silva Mayer <josemayer@usp.br>

---
+++ backend/db/sql/populate_slides.sql
@@ -0,0 +1,14 @@
+INSERT INTO SLIDES (id, small_intro, main_text)
+VALUES (1, 'Bem vindo(a) à', 'Rede GNU-Linux IME-USP');
+
+INSERT INTO SLIDES (id, small_intro, main_text)
+VALUES (2, 'Conta', 'Crie a sua conta');
+
+INSERT INTO SLIDES_BOTOES (id, title, link, color, slide_id)
+VALUES (1, 'Mais informações', '/inspire', 'rgba(5, 22, 30, 1)', 1)
+
+INSERT INTO SLIDES_BOTOES (id, title, link, color, slide_id)
+VALUES (2, 'Logue-se', '/login', 'rgba(5, 22, 30, 1)', 1)
+
+INSERT INTO SLIDES_BOTOES (id, title, link, color, slide_id)
+VALUES (3, 'Clique aqui', 'https://linux.ime.usp.br', 'rgba(5, 22, 30, 1)', 2)
