0de3539d9265e5b488956f43bb785f12664e6df3
fran-dea <102698965+fran-dea@users.noreply.github.com>
2022-12-03 23:42:40 -0300

    modify query

--- backend/services/horarios.js
+++ backend/services/horarios.js
@@ -5,8 +5,8 @@ const config = require('../config');
 async function getMultiple(page = 1) {
  const offset = helper.getOffset(page, config.listPerPage);
  const rows = await db.query(
-    'SELECT * FROM HORARIOS OFFSET $1 LIMIT $2', 
-    [offset, config.listPerPage]
+  'SELECT HORARIOS.id, HORARIOS.weekday, start_time, end_time, admin_id FROM HORARIOS, ADMIN WHERE HORARIOS.admin_id = ADMIN.id OFFSET $1 LIMIT $2', 
+  [offset, config.listPerPage]
   );
   const data = helper.emptyOrRows(rows);
   const meta = {page};
