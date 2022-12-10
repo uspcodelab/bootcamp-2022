9a35e261160ab164e4a7abd3bc2f972bf8aacb4f
José Lucas Silva Mayer <josemayer@usp.br>
2022-12-10 04:00:54 -0300

    Corrige espaçamentos simétricos
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,40 +1,40 @@
 <template>
-  <v-main class="pt-0">
-    <SecaoTitulo title="Login" icon="mdi-account-circle" class="mb-10" />
-    <div class="d-flex align-center justify-center">
+  <div>
+    <SecaoTitulo title="Login" icon="mdi-account-circle" />
+    <div class="d-flex align-center justify-center mt-10">
       <div class="d-flex flex-column align-end">
         <v-row class="d-flex justify-end">
           <v-col cols="3">
-            <h2 class="mt-4">Login</h2>
-            <h2 class="mt-4">Senha</h2>
+            <h2 class="my-2">Login</h2>
+            <h2 class="my-2">Senha</h2>
           </v-col>
 
           <v-col cols="8" class="d-flex flex-column">
             <input
               type="email"
-              class="form-control mt-4 mr-8 pa-4 inputField"
+              class="form-control my-2 mx-8 pa-4 inputField"
               required
             />
             <input
               type="password"
-              class="form-control mt-4 mr-8 pa-4 inputField"
+              class="form-control my-2 mx-8 pa-4 inputField"
               required
             />
           </v-col>
         </v-row>
-        <v-row class="mt-4 mr-8">
+        <v-row class="my-2 mx-8">
           <v-btn solid color="#65AF4B">Entrar</v-btn>
         </v-row>
       </div>
 
       <v-col cols="2" class="d-flex align-center">
-        <v-btn color="#30167C" height="7em" weight="8em">
+        <v-btn color="#30167C" height="7em" width="8em">
           Cadastre-se<br />
           aqui
         </v-btn>
       </v-col>
     </div>
-  </v-main>
+  </div>
 </template>
 
 <script></script>
