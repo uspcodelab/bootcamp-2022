56e44a31672525997d89b282b8f34f77de98ddea
Fran Cristina <franciellyp@usp.br>
2022-12-20 10:56:48 -0300

    apply prettier

--- frontend/components/CalendarioSemanal.vue
+++ frontend/components/CalendarioSemanal.vue
@@ -26,23 +26,31 @@ export default {
   data: () => ({
     today: "2022-12-12",
     events: [],
-    
   }),
   mounted() {
     this.$refs.calendar.scrollToTime("08:00");
   },
   methods: {
     async getHorarios() {
-      await fetch(this.$global.baseUrlApi + "/horarios", { method: "GET" }).then((res) => res.json()).then((data_api) => {
+      await fetch(this.$global.baseUrlApi + "/horarios", { method: "GET" })
+        .then((res) => res.json())
+        .then((data_api) => {
           let horarios_api = data_api.data;
-          let diasDaSemana= {"seg":"2022-12-12", "ter":"2022-12-13", "qua":"2022-12-14", "qui":"2022-12-15", "sex":"2022-12-16", "sab":"2022-12-17"};
-          
+          let diasDaSemana = {
+            seg: "2022-12-12",
+            ter: "2022-12-13",
+            qua: "2022-12-14",
+            qui: "2022-12-15",
+            sex: "2022-12-16",
+            sab: "2022-12-17",
+          };
+
           for (let i = 0; i < horarios_api.length; i++) {
-            let diadasemana = diasDaSemana[horarios_api[i].weekday]
+            let diadasemana = diasDaSemana[horarios_api[i].weekday];
             let evento_novo = {
               name: horarios_api[i].name,
-              start: diadasemana +" " + horarios_api[i].start_time,
-              end: diadasemana +" " + horarios_api[i].end_time,
+              start: diadasemana + " " + horarios_api[i].start_time,
+              end: diadasemana + " " + horarios_api[i].end_time,
             };
             this.events.push(evento_novo);
           }
