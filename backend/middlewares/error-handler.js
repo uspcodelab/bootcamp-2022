325a7360657265b63df9ff295bd821cebd964f88
jppaulo11 <jppaulo11@usp.br>
2023-02-05 17:55:37 -0300

    atualiza cookie secret, retirando as aspas. Ja tive um bug nisso, entao resolvi tirar

--- backend/middlewares/error-handler.js
+++ backend/middlewares/error-handler.js
@@ -1,13 +1,16 @@
 
 const { StatusCodes } = require('http-status-codes');
+const NODE_ENV = require('../config').NODE_ENV
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+  if(NODE_ENV === 'development'){
+    console.log(err)
+  }
   const somethingWrong = require('../errors/error_messages')(req, res).somethingWrong
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     messages: err.messages || [somethingWrong],
   }
-  console.log(err)
   return res
     .status(customError.statusCode)
     .json({ messages: customError.messages })
